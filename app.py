fd779a5ec9946011506192b4ff944ade159d2bd6
Romain Gallé <romain.galle@insa-lyon.fr>
2022-04-13 08:21:54 +0200

    Updated requirements, added SQLAlchemy

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
+import SQLAlchemy as SQLAlchemy
 from dotenv import load_dotenv
 from flask import Flask
+
+from config import configure
 from routes import user_blueprint
 
 load_dotenv()
 
 app = Flask(__name__)
+configure(app)
 app.register_blueprint(user_blueprint)
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
