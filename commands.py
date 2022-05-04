1be1b60f297e1ec68c62d2d5376e97b9826cfb76
Romain Gallé <romain.galle@insa-lyon.fr>
2022-05-04 11:33:20 +0200

    Added dummy db data

--- commands.py
+++ commands.py
@@ -49,6 +49,14 @@ def populate_dummy_db():
                                         timelimit_seconds=None,
                                         user_created=tuthur)
 
+    challenge_3 = challenge_repo.create(subject="Chantez votre chanson favorite",
+                                        title="Karaoké",
+                                        ch_type=ChallengeType.sound,
+                                        start_datetime=datetime.now(),
+                                        end_datetime=datetime(day=12, month=5, year=2022),
+                                        timelimit_seconds=None,
+                                        user_created=tuthur)
+
     challenge_2 = challenge_repo.create(subject="Chofite",
                                         title="Bolopop",
                                         ch_type=ChallengeType.drawing,
@@ -57,13 +65,6 @@ def populate_dummy_db():
                                         timelimit_seconds=None,
                                         user_created=tuthur)
 
-    challenge_3 = challenge_repo.create(subject="Chantez votre chanson favorite",
-                                        title="Karaoké",
-                                        ch_type=ChallengeType.sound,
-                                        start_datetime=datetime.now(),
-                                        end_datetime=datetime(day=12, month=5, year=2022),
-                                        timelimit_seconds=None,
-                                        user_created=tuthur)
 
     collab_challenge = challenge_repo.create_collaborative_challenge(
         subject="Draw what you want",
